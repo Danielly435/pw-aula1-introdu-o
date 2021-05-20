1fb8ab86e15ce8f13f32c404de0cfb41539e1088
Danielly435 <83966729+Danielly435@users.noreply.github.com>
2021-05-20 16:43:03 -0300

    Create olamundo.jsx

---
+++ olamundo.jsx
@@ -0,0 +1,14 @@
+importar  React  de  'react'
+
+exportação  padrão  ( )  =>  {
+    
+    const  informacao  =  'Primeiro componente JSX'
+
+    return  (
+        < >
+
+        < h2 > Componente OlaMundo < / h2 >
+        < p >  {  informacao  }  < / p >
+        < / >
+    )
+}
