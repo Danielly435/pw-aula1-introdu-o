ddaab8f31ddf0cf1d8ba8a41fbb147d0705f2ff0
Danielly435 <83966729+Danielly435@users.noreply.github.com>
2021-05-20 16:15:38 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+
+
+
+class App extends Component {
+  render () {
+    return(
+      <div>
+        <h1>Aula 01-Fundamentos</h1>
+        
+
+      </div>
+
+    )
+  }
+
+}
+
+export default App;
