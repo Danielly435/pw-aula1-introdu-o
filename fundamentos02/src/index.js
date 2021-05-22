dbbd78feda1cbbf46896802afa417cb987b511b9
Danielly435 <danyvitor5@gmail.com>
2021-05-22 13:21:28 +0000

    commit - aula 02

--- fundamentos02/src/index.js
+++ fundamentos02/src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
-//import ReactDOM 'react-dom';
-
+import ReactDOM from 'react-dom';
 import './index.css'
-
 import App from './App';
-
 ReactDOM.render(
-
-	<App />,
-
-document.getElementById('root')
-);
+    <App />,
+  document.getElementById('root')
+)
