12d5ea2efe39b83eb92db237221dcb48d7ba9264
Danielly435 <danyvitor5@gmail.com>
2021-05-24 12:02:19 +0000

    commit- aula02

--- fundamentos02/src/Componentes/basico/Estado.jsx
+++ fundamentos02/src/Componentes/basico/Estado.jsx
@@ -1,15 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export default props => {
 const [curso, setCurso]= useState ('Desenvolvimento de Sistemas')
 const [aluno, setAluno]= useState ('Bartholomew JoJo Simpson')
 
-return (
+return(
     <div>
-        <h3>Curso: {curso} </h3>
-        <h3>Aluno: {aluno} </h3>
+        <h3>Curso: {curso}</h3>
+        <h3>Aluno: {aluno}</h3>
     </div>
-
 )
-
 }
